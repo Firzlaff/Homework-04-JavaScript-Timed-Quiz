8d3bde75797bd9a55dde72bab0419655a414b3fa
Firzlaff <tysenfirzlaff@gmail.com>
2020-03-22 09:38:24 -0600

    Sudoing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
-// will need a variable for the answers/score 
+// Variable for the answers/score 
 
-// when i click the start game button the timer starts and the questions are started
+// when i click the start game button the timer starts and the questions are started, event listener that starts time and switchs to questions
 
-//if you answer correctly your score goes up if you answer incorrectly nothing happens
+// As questions are asked if you answer correctly your score goes up if you answer incorrectly no score is added
 
-// when the timer ends or the questions have all been completed then the game ends
+// when the timer ends or the questions have all been completed then the game ends and you move to end game screen.
+// ^Event listener for when timer hits 0/ max time hits 0 or all questions are answered
+//  ^the answers or scores must be saved locallaly to store in the high scores. 
 
-// the answers or scores must be saved locallaly to store in the high scores. 
+// On the end-game screen you are prompted to enter your initials to save to the score/high-score 
+// On the end-game screen you then can click to play again or to view the high-scores  // maybe this can just presist through thoughout like a restart..
 
-// when the game ends you are are prompted to enter your initials to save to the high-scores 
-
-// you then can click to play again or to view the high-scores 
-
-// from the high-scores you can play again or clear the scores
+// if you clicked high-scores you are taken to high-score section
+// from the high-scores you see the stored high scores with initials that were saved, 
+// from the high-scores sections there are two buttons play again or clear the high-scores

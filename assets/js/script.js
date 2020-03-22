3826e06fa00e10ebf57ed7d2e40f64a507fc9118
Firzlaff <tysenfirzlaff@gmail.com>
2020-03-22 09:20:47 -0600

    creating folder/files html and js sudo

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// will need a variable for the answers/score 
+
+// when i click the start game button the timer starts and the questions are started
+
+//if you answer correctly your score goes up if you answer incorrectly nothing happens
+
+// when the timer ends or the questions have all been completed then the game ends
+
+// the answers or scores must be saved locallaly to store in the high scores. 
+
+// when the game ends you are are prompted to enter your initials to save to the high-scores 
+
+// you then can click to play again or to view the high-scores 
+
+// from the high-scores you can play again or clear the scores

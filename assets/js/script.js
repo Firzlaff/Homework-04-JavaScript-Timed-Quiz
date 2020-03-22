bb139de4780342ae1e163142d8e834511e1fbfe9
Firzlaff <tysenfirzlaff@gmail.com>
2020-03-22 14:43:18 -0600

    finishing html and finishing sudo and starting to add a little JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,24 @@
 // Variable for the answers/score 
 var Score;
 
-// when i click the start game button the timer starts and the questions are started, event listener that starts time and switchs to questions
+// when the page loads I am looking at the Start Screen for the game which shows the title a click below to play now and the button for play now and high-scores
+
+// when i click the start playnow button the timer starts and the questions are started, event listener that starts time and switchs to questions
+$("#play-Btn")click
 
 // As questions are asked if you answer correctly your score goes up if you answer incorrectly no score is added
 
+
+
 // when the timer ends or the questions have all been completed then the game ends and you move to end game screen.
 // ^Event listener for when timer hits 0/ max time hits 0 or all questions are answered
-//  ^the answers or scores must be saved locallaly to store in the high scores. 
+//  ^the answers or scores must be saved locallaly to store in the high scores. JSON.stringify to stringify it for save JSON.parse to save it to the local storage
+
 
 // On the end-game screen you are prompted to enter your initials to save to the score/high-score 
-// On the end-game screen you then can click to play again or to view the high-scores  // maybe this can just presist through thoughout like a restart..
+// On the end-game screen you then can click to play again or to view the high-scores  // maybe this can just presist thoughout like a restart..
+
+
 
 // if you clicked high-scores you are taken to high-score section
 // from the high-scores you see the stored high scores with initials that were saved, 

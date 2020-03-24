4735659802eeb860751d8c89ddaf0a115f1aed6e
Firzlaff <tysenfirzlaff@gmail.com>
2020-03-24 00:53:43 -0600

    updating html adding spots for displays I will need, added more html nodes and starting eventlisteners and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,29 @@
 
 
-
-
 $(document).ready(function () {
 
 
-// HTML Nodes
+// HTML Nodes  
 
 let questionsDisplay = $("#questions");
-let startBtn = $("#playNow-Btn");
+let playNowBtn = $("#playNow-Btn");
 let quizTitleDisplay = $("#quiz-title");
 let quesitonsDisplayBox = $("#questions-display-box");
 let highScoresDisplay = $("#hihgScores-Btn");
-let startBtn = $("#");
-let startBtn = $("#");
+let timerDisplay = $("#timer");
+let answerResults = $("#answer-Results-Display-Box");
 let startBtn = $("#");
 
 
 // Variable for the answers/score 
 let score = 0;
-//var quesitons;
+let btn1
+//Other variables var quesitons;
 let choices;
 
-// questions stored as an object with the question, choices and answer to the question. 
+// questions stored as an object with questions, choices and answers
 
-var questions = [
+let questions = [
     {
         question: "How does a For Loop start",
         choice1: "for (i<=5)",
@@ -90,36 +89,43 @@ var questions = [
   ];
 
    
-// when i click the start playnow button the timer starts and the questions are started, event listener that starts time and switchs to questions
+// when i click the playnow button the timer starts  
 //$("#play-Btn")click
-
-
-
 // Event listner for event click to change to questions 
-startBtn.addEventListener("click", questionsGenerator(event));{
+playNowBtn.on("click", questionsGenerator(event));{
+
+// when i click the playnow button the timer starts  
+//$("#play-Btn")click
+TimerDisplay
+
 
 // A question is asked with 4 multiple chioce answers once the user clicks an options it choses that answer
 //ask the quetsions
-function questionsGenerator(){
-    $("#questions").change
+//function questionsGenerator(){
+ Funciton questionGenerator() = $("#questions").text = 
+} 
+// Buttons to load the choices into for selection which 
+//I feel I will need to use a forLoop that cycles all questions and choices returns the number of correctly selected answers
+//let generateQuestions = $(this.text) 
 
-// Buttons to load the choices into for selection which i feel I will need to use a for loop somehow 
-// Buttons to load the choices into for selection which i feel I will need to use a for loop somehow 
-var btn1 = document.createElement("button");
-button.innerHTML = "choice1";
+let btn1 = $("<button>").$(this)text = choice1,;
 
-var btn2 = document.createElement("button");
-button.innerHTML = "choice2";
 
-var btn3 = document.createElement("button");
-button.innerHTML = "chioce3";
+//var btn2 = document.createElement("button");
+//button.innerHTML = "choice2";
 
-var btn4 = document.createElement("button");
-button.innerHTML = "choice4";
+//var btn3 = document.createElement("button");
+//button.innerHTML = "chioce3";
+
+//var btn4 = document.createElement("button");
+//button.innerHTML = "choice4";
 };
+
+
 // log the answer if correct 
 //keep track of the correct answers
 // the total number of correct answers becomes the total score. 
+
 
 // when the timer ends or the questions have all been completed then the game ends and you move to end game screen.
 // ^Event listener for when timer hits 0/ max time hits 0 or all questions are answered
@@ -129,9 +135,10 @@ button.innerHTML = "choice4";
 // On the end-game screen you are prompted to enter your initials to save to the score/high-score 
 // On the end-game screen you then can click to play again or to view the high-scores  // maybe this can just presist thoughout like a restart..
 
+
 // if you clicked high-scores you are taken to high-score section
 // from the high-scores you see the stored high scores with initials that were saved, 
 // from the high-scores sections there are two buttons play again or clear the high-scores
 
 
-}
+})
